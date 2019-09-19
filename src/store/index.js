2a0ddec6af5bbd2ad83c1c95f036510d1554929c
Alan Murad <alan.murad42@gmail.com>
2019-09-19 18:11:37 +0100

    Change months with Vuex mutation

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,10 @@ export default new Vuex.Store({
     state: {
       currentYear: 2019,
       currentMonth: 9
+    },
+    mutations: {
+        setCurrentMonth(state, payload) {
+            state.currentMonth = payload;
+        }
     }
 });
