96a52c6dd5ee7e5be755a4e77c47bcb814e2ad68
Alan Murad <alan.murad42@gmail.com>
2019-09-19 14:30:46 +0100

    Padding days to start/end of month for complete weeks

--- src/components/App.vue
+++ src/components/App.vue
@@ -8,18 +8,44 @@
     export default {
         data() {
             return {
-                month: 2,
+                month: 6,
                 year: 2019
             }; 
         },
         computed: {
             days() {
+                // Generate all  days in current month
                 let days = [];
                 let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
                 do {
                     days.push(currentDay);
                     currentDay = this.$moment(currentDay).add(1, 'days');
                 } while ((currentDay.month() + 1) === this.month);
+
+                // Add previous days to start
+                currentDay = this.$moment(days[0]);
+
+                const SUNDAY = 0
+                const MONDAY = 1
+
+                if (currentDay.day() !== MONDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).subtract(1, 'days');
+                        days.unshift(currentDay);
+                    } while(currentDay.day() !== MONDAY);
+                }
+                
+                // Add following days to the end
+                currentDay = this.$moment(days[days.length - 1])
+                
+                if (currentDay.day() !== SUNDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).add(1, 'days');
+                        days.push(currentDay);
+                    } while(currentDay.day() !== SUNDAY);    
+                }
+                
+
                 return days;
             }
         }   
